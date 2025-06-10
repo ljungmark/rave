e0bd4730edd3dcf42c29a5c1cf54a4cd6b09ee79
Ljungmark <mattias@ljungmark.org>
2025-06-10 20:32:44 +0200

    Identify number of network requests required to load entries
    Determine how many network requests are required to load all of the requested entries.

--- rave.js
+++ rave.js
@@ -1,2 +1,20 @@
-/* Throttle actions to protect against threshold limits. */
+const rave = async (entries) => {
+    /* Determine how many network requests are required to load all of the requested entries.
+
+       The initial page load contain 15 check-ins.
+       Each subsequent request for more check-ins contains 15 additional entries.
+
+       Expected results:
+       entries | result
+       1         0
+       15        0
+       16        1
+       30        1
+       31        2
+       45        2
+       46        3, etc. */
+    const requests = Math.floor((entries - 1) / 15);
+};
+
+/* Throttle actions to protect against rate limit thresholds. */
 const throttle = ms => new Promise(resolve => setTimeout(resolve, ms));
