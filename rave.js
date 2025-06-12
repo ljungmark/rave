f62a4a2c01971ff21c5ca48ba5d4b93aa24fe8e6
Ljungmark <mattias@ljungmark.org>
2025-06-12 21:09:34 +0200

    Perform return, elaborate on intended results

--- rave.js
+++ rave.js
@@ -62,11 +62,22 @@ const rave = async (entries = 15) => {
 /* Throttle actions to protect against rate limit thresholds. */
 const throttle = (base = 1000) => {
     /* Randomize a number based on the `base` argument, +/- 1000.
-       The value will never go below 1000. */
+       The value will never go below 1000.
+
+       Expected results:
+       base | delay
+       50     1000-1050
+       500    1000-1500
+       1000   1000-2000
+       1500   1000-2500
+       2000   1000-3000
+       3000   2000-4000
+       5000   4000-60000, etc. */
     const min = Math.max(1000, base - 1000);
     const max = base + 1000;
+    const delay =  Math.floor(Math.random() * (max - min + 1)) + min;
 
-    new Promise(resolve => setTimeout(resolve, Math.floor(Math.random() * (max - min + 1)) + min));
+    return new Promise(resolve => setTimeout(resolve, delay));
 }
 
 rave(60);
