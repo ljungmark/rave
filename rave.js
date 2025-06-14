871176bb1f1c60b962462cd606be9450385f6a9f
Ljungmark <mattias@ljungmark.org>
2025-06-14 20:32:09 +0200

    Limit stimuli to only be applied on select sensors

--- rave.js
+++ rave.js
@@ -38,6 +38,24 @@ const buffer = async (requests, allowance = 3) => {
     console.info('Buffer directive completed.');
 };
 
+const toast = async (sensors) => {
+    const volume = sensors.length;
+    for (let iteration = 1; iteration <= volume; iteration++) {
+        /* Every fifth stimuli, throttle by ~5000ms, otherwise
+           throttle for ~1000ms. This is to not breach rate limit
+           thresholds. */
+        const delay = iteration % 5 === 0 ? 5000 : 1000;
+        await throttle(delay);
+
+        try {
+            sensors[iteration-1].click();
+            console.info(`Toasted ${iteration}/${volume}!`);
+        } catch (error) {
+            console.error(`Failed to toast ${iteration}/${volume}:`, error);
+        }
+    }
+};
+
 const rave = async (entries = 15) => {
     /* Determine how many network requests are required to load all of the requested entries.
 
@@ -59,26 +77,10 @@ const rave = async (entries = 15) => {
     await buffer(requests);
 
     await throttle(2000);
-    const sensors = document.querySelectorAll('.toast_btn .toast:not(.active)');
-    toast(sensors);
-};
 
-const toast = async (sensors) => {
-    const volume = sensors.length;
-    for (let iteration = 1; iteration <= volume; iteration++) {
-        /* Every fifth stimuli, throttle by ~5000ms, otherwise
-           throttle for ~1000ms. This is to not breach rate limit
-           thresholds. */
-        const delay = iteration % 5 === 0 ? 5000 : 1000;
-        await throttle(delay);
-
-        try {
-            sensors[iteration-1].click();
-            console.info(`Toasted ${iteration}/${volume}!`);
-        } catch (error) {
-            console.error(`Failed to toast ${iteration}/${volume}:`, error);
-        }
-    }
+    /* Array.from() returns a NodeList, which has the .slice() method on it. */
+    const sensors = Array.from(document.querySelectorAll('.toast_btn .toast:not(.active)'));
+    toast(sensors.length > entries ? sensors.slice(0, entries) : sensors);
 };
 
 /* Throttle actions to protect against rate limit thresholds. */
